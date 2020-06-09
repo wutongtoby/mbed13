ce5397752ec9dd2ea0e40d4eb407dfe18d4bd990
wutong <wutongtoby@gmail.com>
2020-06-09 15:55:30 +0800

    e

---
+++ 13_1_Simple_test/main.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+#include "bbcar.h"
+
+Ticker servo_ticker;
+PwmOut pin9(D9), pin8(D8);
+BBCar car(pin8, pin9, servo_ticker);
+
+int main(void) 
+{
+    while (1) {
+        car.goStraight(100);
+        wait(1);
+        car.stop();
+        wait(1);
+    }
+
+}
